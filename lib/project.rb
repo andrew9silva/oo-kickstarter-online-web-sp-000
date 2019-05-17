bd9ca2c07ce3a8c275514827856d6b0d3868ce1a
Andrew Silva <andrewsilva9@gmail.com>
2019-05-17 02:17:21 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
   end
 
   def add_backer(backer)
-    @backers << backer 
+    @backers << backer
     backer.back_project(self) unless backer..backed_projects.include?(self)
   end
 end
